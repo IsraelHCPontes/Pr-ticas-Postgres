4c641c0d87b8da4277fc79038275b87607364140
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-07 22:34:15 -0300

    feat(criacao)

---
+++ Prática - Exercícios SQL/11.sql
@@ -0,0 +1,4 @@
+SELECT id FROM clientes WHERE nome='Martinha Lima Zambujal';
+SELECT id FROM produtos WHERE nome='Fone Topo de Linha';
+SELECT id FROM compras WHERE id_cliente=5 AND id_produto=14;
+DELETE FROM compras WHERE id = 6;
