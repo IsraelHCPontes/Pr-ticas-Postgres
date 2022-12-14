6dec0cc25e8be2543ef39edda01f9bac189ef501
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-14 19:38:23 -0300

    fix(Exercício_01)

--- Praticas-Exercícios Join/01.sql
+++ Praticas-Exercícios Join/01.sql
@@ -1,1 +1,5 @@
-SELECT * FROM public.users JOIN public.cities ON public.users."cityId" = public.cities.id WHERE cities.name = 'Rio de Janeiro';
+SELECT public.users.id, public.users.name, public.cities.name id 
+FROM public.users 
+JOIN public.cities 
+ON public.users."cityId" = public.cities.id 
+WHERE cities.name = 'Rio de Janeiro';
