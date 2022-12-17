f64e336d2ac8478eb55b451fc2d081e30d52ef74
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-17 14:11:56 -0300

    feat(Exrcicio_03)

--- Praticas-Exercícios Join/03.sql
+++ Praticas-Exercícios Join/03.sql
@@ -1,6 +1,6 @@
  SELECT testimonials.id ,  u1.name writer, u2.name recipient, testimonials.message
- FROM  public.testimonials testimonials 
- JOIN public.users u1
+ FROM   testimonials 
+ JOIN users u1
  ON testimonials."writerId" = u1.id
- JOIN public.users u2 
+ JOIN users u2 
  ON testimonials."recipientId" = u2. id
