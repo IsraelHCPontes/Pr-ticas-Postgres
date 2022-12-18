b0e871c5a3f66dae7ba1a358124e9b90f723a288
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-18 15:39:23 -0300

    feat(Exercício_05)

---
+++ Prática - Exercícios Group By/05.sql
@@ -0,0 +1,9 @@
+SELECT 
+	MAX(salary) AS "maximumSalary",
+	roles.name AS role
+FROM jobs
+JOIN roles ON jobs."roleId" = roles.id
+WHERE jobs.active = true
+GROUP BY roles.name 
+ORDER BY "maximumSalary" 
+
